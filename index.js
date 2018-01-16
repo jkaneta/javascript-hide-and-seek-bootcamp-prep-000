47afdf8b6dcc9a77bdc5da0d46b0afc6dd5073ee
J <flatironschool@2joyce.com>
2018-01-16 04:32:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div.target');
+}
+
+function increaseRankBy(n) {
+  var myList = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (var item in myList) {
+    myList[item].innerHTML = parseInt(myList[item].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  let currentSelector = document.querySelector('div#grand-node');
+  let nextSelector = currentSelector;
+  let elementTagName = '';
+  
+  while (currentSelector) {
+    elementTagName = currentSelector.tagName;
+    nextSelector = currentSelector.querySelector(elementTagName);
+    if (nextSelector === null) {
+      return currentSelector;
+    }
+    currentSelector = nextSelector;
+  }
+  return null;
+}
