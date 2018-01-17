d53725f75b788141f8a7cf5c3059a124807305fb
J <flatironschool@2joyce.com>
2018-01-17 19:09:27 +0000

    Done.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return document.getElementById('nested').querySelector('div.target');
+  return document.getElementById('nested').querySelector('.target');
 }
 
 function increaseRankBy(n) {
